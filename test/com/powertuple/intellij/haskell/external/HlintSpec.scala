b7fb58eb3fbb7e42103a001af91d1af017ece1d5
Rik <rikvdkleij@gmaikl.com>
2014-10-25 16:19:36 +0200

    What's in a test description :-)

--- test/com/powertuple/intellij/haskell/external/HlintSpec.scala
+++ test/com/powertuple/intellij/haskell/external/HlintSpec.scala
@@ -4,20 +4,21 @@ import org.scalatest.{FunSpec, GivenWhenThen, Matchers}
 
 class HlintSpec extends FunSpec with Matchers with GivenWhenThen {
 
-  describe("Parsing hlint output") {
-    Given("type info output of ghc-modi")
+  describe("Parsing HLint output") {
+    Given("output of HLint")
     val output =
       """
       [{"module":"SayHello","decl":"sayHello","severity":"Error","hint":"Redundant if","file":"src/Effe.hs","startLine":17,"startColumn":11,"endLine":17,"endColumn":44,"from":"if True then getLine else getLine","to":"getLine","note":["\"increases laziness\""]}
       ,{"module":"SayHello","decl":"x","severity":"Error","hint":"Redundant if","file":"src/Effe.hs","startLine":34,"startColumn":5,"endLine":34,"endColumn":33,"from":"if True then True else False","to":"True","note":[]}]
       """
 
-    When("converted to list of type info")
+    When("converted to list of hlint infos")
     val hlintInfos = Hlint.deserializeHlintInfo(output)
 
     Then("it should contain right info")
 
     hlintInfos should have size 2
+
     val hlintInfo1 = hlintInfos(0)
     hlintInfo1.startLine shouldEqual 17
     hlintInfo1.note.size shouldEqual 1
