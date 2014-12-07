a094caeb69560c425d000e48b29ba91009e6ca92
rikvdkleij <rikvdkleij@gmail.com>
2014-12-07 16:50:03 +0100

    Improved code completion.

--- src/com/powertuple/intellij/haskell/code/HaskellCompletionContributor.scala
+++ src/com/powertuple/intellij/haskell/code/HaskellCompletionContributor.scala
@@ -20,6 +20,7 @@ import java.util.concurrent.{Executors, TimeUnit}
 
 import com.intellij.codeInsight.completion._
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.impl.source.tree.TreeUtil
@@ -29,8 +30,10 @@ import com.intellij.util.ProcessingContext
 import com.powertuple.intellij.haskell.external.{BrowseInfo, GhcMod}
 import com.powertuple.intellij.haskell.psi.HaskellTypes._
 import com.powertuple.intellij.haskell.psi._
+import com.powertuple.intellij.haskell.util.LineColumnPosition
 import com.powertuple.intellij.haskell.{HaskellIcons, HaskellParserDefinition}
 
+import scala.annotation.tailrec
 import scala.collection.JavaConversions._
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -47,16 +50,18 @@ class HaskellCompletionContributor extends CompletionContributor {
   private final val ReservedIds = HaskellParserDefinition.ALL_RESERVED_IDS.getTypes.map(_.asInstanceOf[HaskellTokenType].getName).toSeq
   private final val SpecialReservedIds = Seq("forall", "safe", "unsafe")
   private final val PragmaIds = Seq("{-#", "LANGUAGE", "#-}", "{-# LANGUAGE")
-  private final val InsideImportClauses = Seq("as ", "hiding ", "qualified ")
+  private final val InsideImportClauses = Seq("as", "hiding", "qualified")
+  private final val CommentIds = Seq("{-", "-}", "--")
+
+  private final val PlainPrefixElementTypes = Seq(HS_COMMENT, HS_NCOMMENT, HS_PRAGMA_START, HS_PRAGMA_END)
 
   extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new CompletionProvider[CompletionParameters] {
     def addCompletions(parameters: CompletionParameters, context: ProcessingContext, originalResultSet: CompletionResultSet) {
-
       val project = parameters.getPosition.getProject
       val file = parameters.getOriginalFile
 
-      val resultSet = Option(parameters.getPosition).orElse(Option(parameters.getOriginalPosition)) match {
-        case Some(p) if p.getText.startsWith("{") => originalResultSet.withPrefixMatcher(new PlainPrefixMatcher(p.getText))
+      val resultSet = getNonEmptyElement(parameters.getOriginalPosition).orElse(getNonEmptyElement(parameters.getPosition)) match {
+        case Some(e) if PlainPrefixElementTypes.contains(e.getNode.getElementType) | e.getText.startsWith("{") | e.getText.startsWith("#") | e.getText.startsWith("-") => originalResultSet.withPrefixMatcher(new PlainPrefixMatcher(getTextAtCaret(parameters.getEditor, file)))
         case _ => originalResultSet
       }
 
@@ -70,10 +75,14 @@ class HaskellCompletionContributor extends CompletionContributor {
         case Some(p) if isPragmaInProgress(p) =>
           resultSet.addAllElements(getLanguageExtensions(project))
           resultSet.addAllElements(getPragmaIds)
+        case Some(p) if isNCommentInProgress(p) =>
+          resultSet.addAllElements(getPragmaIds)
+          resultSet.addAllElements(getCommentIds)
         case _ =>
           resultSet.addAllElements(getReservedIds)
           resultSet.addAllElements(getSpecialReservedIds)
           resultSet.addAllElements(getPragmaIds)
+          resultSet.addAllElements(getCommentIds)
           resultSet.addAllElements(getIdsFromFullScopeImportedModules(project, file))
           resultSet.addAllElements(getIdsFromHidingIdsImportedModules(project, file))
           resultSet.addAllElements(getIdsFromSpecIdsImportedModules(project, file))
@@ -84,16 +93,43 @@ class HaskellCompletionContributor extends CompletionContributor {
   override def beforeCompletion(context: CompletionInitializationContext) {
     val file = context.getFile
     val startOffset = context.getStartOffset
-    val offSet = if (startOffset > 0) {
-      startOffset - 1
-    } else {
-      startOffset
+
+    val lineColumnPosition = LineColumnPosition.fromOffset(file, startOffset)
+    val caretElement = lineColumnPosition match {
+      case Some(lcp) => if (lcp.columnNr == 1) {
+        getNonEmptyElement(file.findElementAt(startOffset))
+      } else {
+        getNonEmptyElement(file.findElementAt(startOffset)).orElse(getNonEmptyElement(file.findElementAt(startOffset - 1)))
+      }
+      case _ => None
     }
 
-    val caretElement = Option(file.findElementAt(offSet))
     caretElement match {
-      case Some(e) if e.getText.trim.size > 0 => context.setDummyIdentifier(e.getText.substring(0, 1))
+      case Some(e) if PlainPrefixElementTypes.contains(e.getNode.getElementType) => context.setDummyIdentifier(getTextAtCaret(context.getEditor, file))
+      case Some(e) if e.getText.trim.size > 0 => context.setDummyIdentifier(e.getText.trim)
       case _ => context.setDummyIdentifier("a")
+    }
+  }
+
+  private def getTextAtCaret(editor: Editor, file: PsiFile): String = {
+    val caretOffset = editor.getCaretModel.getOffset
+    getTextUntilNoChar(file.getText, if (caretOffset > 0) caretOffset - 1 else caretOffset, "")
+  }
+
+  @tailrec
+  private def getTextUntilNoChar(fileText: String, offset: Int, text: String): String = {
+    val c = fileText.charAt(offset)
+    if (c < ' ' || c == '\n' || c == '\r') {
+      text
+    } else {
+      getTextUntilNoChar(fileText, offset - 1, c.toString + text)
+    }
+  }
+
+  private def getNonEmptyElement(element: PsiElement) = {
+    Option(element) match {
+      case Some(s) if !s.getText.trim.isEmpty => Some(s)
+      case _ => None
     }
   }
 
@@ -120,22 +156,28 @@ class HaskellCompletionContributor extends CompletionContributor {
   }
 
   private def getInsideImportClauses = {
-    InsideImportClauses.map(c => LookupElementBuilder.create(c).withTailText(" clause", true))
+    InsideImportClauses.map(c => LookupElementBuilder.create(c + " ").withTailText(" clause", true))
   }
 
   private def isPragmaInProgress(position: PsiElement): Boolean = {
-    position.getNode.getElementType == HS_NCOMMENT ||
-        Option(TreeUtil.findSiblingBackward(position.getNode, HS_PRAGMA_START)).
-            orElse(Option(TreeUtil.findSibling(position.getNode, HS_PRAGMA_END))).
-            orElse(Option(TreeUtil.findSiblingBackward(position.getParent.getNode, HS_PRAGMA_START))).isDefined
+    Option(TreeUtil.findParent(position.getNode, HaskellTypes.HS_LANGUAGE_PRAGMAS)).orElse(
+      Option(TreeUtil.findParent(position.getNode, HaskellTypes.HS_LANGUAGE_PRAGMA))).isDefined
+  }
+
+  private def isNCommentInProgress(position: PsiElement): Boolean = {
+    position.getNode.getElementType == HS_NCOMMENT
   }
 
   private def getLanguageExtensions(project: Project) = {
-    GhcMod.listLanguageExtensions(project).map(n => LookupElementBuilder.create(n).withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" language extension", true))
+    GhcMod.listLanguageExtensions(project).map(n => LookupElementBuilder.create(n + " ").withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" language extension", true))
   }
 
   private def getPragmaIds: Seq[LookupElementBuilder] = {
-    PragmaIds.map(p => LookupElementBuilder.create(p).withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" pragma", true))
+    PragmaIds.map(p => LookupElementBuilder.create(p + " ").withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" pragma", true))
+  }
+
+  private def getCommentIds: Seq[LookupElementBuilder] = {
+    CommentIds.map(p => LookupElementBuilder.create(p + " ").withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" comment", true))
   }
 
   private def getImportedModulesWithFullScope(psiFile: PsiFile): Iterable[ImportFullSpec] = {
@@ -220,11 +262,11 @@ class HaskellCompletionContributor extends CompletionContributor {
   }
 
   private def getReservedIds = {
-    ReservedIds.map(r => LookupElementBuilder.create(r).withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" keyword", true))
+    ReservedIds.map(r => LookupElementBuilder.create(r + " ").withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" keyword", true))
   }
 
   private def getSpecialReservedIds = {
-    SpecialReservedIds.map(sr => LookupElementBuilder.create(sr).withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" special keyword", true))
+    SpecialReservedIds.map(sr => LookupElementBuilder.create(sr + " ").withIcon(HaskellIcons.HaskellSmallBlueLogo).withTailText(" special keyword", true))
   }
 
   private def findImportDeclarations(psiFile: PsiFile) = {
@@ -283,6 +325,5 @@ class HaskellCompletionContributor extends CompletionContributor {
   private case class BrowseInfosForImportHidingIdsSpec(importSpec: ImportHidingIdsSpec, browseInfos: Seq[BrowseInfo])
 
   private case class BrowseInfosForImportIdsSpec(importSpec: ImportIdsSpec, browseInfos: Seq[BrowseInfo])
-
 
 }
