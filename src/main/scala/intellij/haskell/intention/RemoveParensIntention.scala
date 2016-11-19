655eb0d7b61e53000d0bb3f303dd04521ef4958b
zjhmale <zjhmale@gmail.com>
2016-11-19 09:28:07 +0800

    Remove parens intention

--- src/main/scala/intellij/haskell/intention/RemoveParensIntention.scala
+++ src/main/scala/intellij/haskell/intention/RemoveParensIntention.scala
@@ -3,29 +3,19 @@ package intellij.haskell.intention
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{PsiElement, PsiFileFactory}
+import com.intellij.psi.PsiElement
+import intellij.haskell.HaskellNotificationGroup
 import intellij.haskell.psi.HaskellTypes._
-import intellij.haskell.{HaskellLanguage, HaskellNotificationGroup}
 
-import scala.collection.JavaConversions._
-
-class AddParensIntention extends PsiElementBaseIntentionAction {
+class RemoveParensIntention extends PsiElementBaseIntentionAction {
   override def invoke(project: Project, editor: Editor, psiElement: PsiElement): Unit = {
-    val parens = PsiTreeUtil.findChildrenOfType(PsiFileFactory.getInstance(project).createFileFromText("DUMMY.hs", HaskellLanguage.Instance, "add = (1 + 2)"), classOf[LeafPsiElement])
-    val left = parens.filter(_.getNode.getElementType == HS_LEFT_PAREN).head
-    val right = parens.filter(_.getNode.getElementType == HS_RIGHT_PAREN).head
-    HaskellNotificationGroup.logWarning(s"============ $left")
-    HaskellNotificationGroup.logWarning(s"============ $right")
-    HaskellNotificationGroup.logWarning(s"============ psi element text ${psiElement.getText}")
     val psiFile = psiElement.getContainingFile
     val start = psiFile.findElementAt(editor.getSelectionModel.getSelectionStart)
     val end = psiFile.findElementAt(editor.getSelectionModel.getSelectionEnd - 1)
     HaskellNotificationGroup.logWarning(s"============ start element text ${psiElement.getText}")
     HaskellNotificationGroup.logWarning(s"============ end element text ${psiElement.getText}")
-    start.getParent.addBefore(left, start)
-    end.getParent.addAfter(right, end)
+    start.delete()
+    end.delete()
   }
 
   override def isAvailable(project: Project, editor: Editor, psiElement: PsiElement): Boolean = {
@@ -33,10 +23,10 @@ class AddParensIntention extends PsiElementBaseIntentionAction {
     val start = psiFile.findElementAt(editor.getSelectionModel.getSelectionStart)
     val end = psiFile.findElementAt(editor.getSelectionModel.getSelectionEnd - 1)
     HaskellNotificationGroup.logWarning(s"=============== ${start.getNode.getElementType == HS_LEFT_PAREN} ${psiElement.isWritable} ${end.getNode.getElementType == HS_RIGHT_PAREN}")
-    psiElement.isWritable && start.getNode.getElementType != HS_LEFT_PAREN && end.getNode.getElementType != HS_RIGHT_PAREN
+    psiElement.isWritable && start.getNode.getElementType == HS_LEFT_PAREN && end.getNode.getElementType == HS_RIGHT_PAREN
   }
 
   override def getFamilyName: String = getText
 
-  override def getText: String = "Add parens around expression"
+  override def getText: String = "Remove parens around expression"
 }
