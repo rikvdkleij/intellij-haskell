93f4568ac999e52a6377abeffacb7e3ae266f90e
Rik van der Kleij <rikvdkleij@gmail.com>
2016-12-09 15:47:38 +0100

    Do not create notification when project is already disposed. Refactor.

--- src/main/scala/intellij/haskell/HaskellNotificationGroup.scala
+++ src/main/scala/intellij/haskell/HaskellNotificationGroup.scala
@@ -16,7 +16,7 @@
 
 package intellij.haskell
 
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.{Notification, NotificationGroup}
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MessageType
 import com.intellij.openapi.ui.MessageType._
@@ -27,35 +27,35 @@ object HaskellNotificationGroup {
   private val BalloonGroup = NotificationGroup.balloonGroup("Balloon Haskell")
 
   def logErrorEvent(project: Option[Project], message: String) {
-    logEvent(project, message, ERROR)
+    logEvent(project, message, ERROR, LogOnlyGroup.createNotification)
   }
 
   def logErrorEvent(project: Project, message: String) {
-    logEvent(Option(project), message, ERROR)
+    logEvent(Option(project), message, ERROR, LogOnlyGroup.createNotification)
   }
 
   def logErrorEvent(message: String) {
-    logEvent(None, message, ERROR)
+    logEvent(None, message, ERROR, LogOnlyGroup.createNotification)
   }
 
   def logWarningEvent(project: Project, message: String) {
-    logEvent(Option(project), message, WARNING)
+    logEvent(Option(project), message, WARNING, LogOnlyGroup.createNotification)
   }
 
   def logWarningEvent(message: String) {
-    logEvent(None, message, WARNING)
+    logEvent(None, message, WARNING, LogOnlyGroup.createNotification)
   }
 
   def logInfoEvent(project: Option[Project], message: String) {
-    logEvent(project, message, INFO)
+    logEvent(project, message, INFO, LogOnlyGroup.createNotification)
   }
 
   def logInfoEvent(project: Project, message: String) {
-    logEvent(Option(project), message, INFO)
+    logEvent(Option(project), message, INFO, LogOnlyGroup.createNotification)
   }
 
   def logInfoEvent(message: String) {
-    logEvent(None, message, INFO)
+    logEvent(None, message, INFO, LogOnlyGroup.createNotification)
   }
 
   def logErrorBalloonEvent(project: Option[Project], message: String) {
@@ -90,13 +90,19 @@ object HaskellNotificationGroup {
     balloonEvent(None, message, INFO)
   }
 
-  //noinspection UnitInMap
-  private def logEvent(project: Option[Project], message: String, messageType: MessageType) = {
-    project.map(p => LogOnlyGroup.createNotification(message, messageType).notify(p)).getOrElse(LogOnlyGroup.createNotification(message, messageType).notify())
+  private def logEvent(project: Option[Project], message: String, messageType: MessageType, notification: (String, MessageType) => Notification) = {
+    log(project, message, messageType, LogOnlyGroup.createNotification)
   }
 
-  //noinspection UnitInMap
   private def balloonEvent(project: Option[Project], message: String, messageType: MessageType) = {
-    project.map(p => BalloonGroup.createNotification(message, messageType).notify(p)).getOrElse(BalloonGroup.createNotification(message, messageType).notify())
+    log(project, message, messageType, BalloonGroup.createNotification)
+  }
+
+  private def log(project: Option[Project], message: String, messageType: MessageType, notification: (String, MessageType) => Notification) = {
+    project match {
+      case Some(p) if !p.isDisposed => notification(message, messageType).notify(p)
+      case None => notification(message, messageType).notify()
+      case _ => ()
+    }
   }
 }
