31e843616c976ac00be8f085a654e06b424f3ce0
zjhmale <zjhmale@gmail.com>
2017-01-05 17:03:47 +0800

    Remove duplicate typo hint

--- src/main/scala/intellij/haskell/spellchecker/HaskellSpellcheckingStrategy.scala
+++ src/main/scala/intellij/haskell/spellchecker/HaskellSpellcheckingStrategy.scala
@@ -8,10 +8,5 @@ import com.intellij.spellchecker.tokenizer.{Tokenizer, SpellcheckingStrategy}
  * Provide spellchecker support for Haskell sources.
  */
 class HaskellSpellcheckingStrategy extends SpellcheckingStrategy {
-  // Use TEXT_TOKENIZER so prime' names won't be marked as a typo.
-  override def getTokenizer(element: PsiElement): Tokenizer[_ <: PsiElement] = {
-    SpellcheckingStrategy.TEXT_TOKENIZER
-  }
-
   override def isMyContext(element: PsiElement): Boolean = HaskellLanguage.Instance.is(element.getLanguage)
 }
