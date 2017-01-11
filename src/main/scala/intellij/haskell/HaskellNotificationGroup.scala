e079e5e4d04b15e428360bcd263d7be739a156d9
Rik van der Kleij <rikvdkleij@gmail.com>
2017-01-11 15:03:36 +0100

    We want to call IntelliJ's notify :-)

--- src/main/scala/intellij/haskell/HaskellNotificationGroup.scala
+++ src/main/scala/intellij/haskell/HaskellNotificationGroup.scala
@@ -101,7 +101,7 @@ object HaskellNotificationGroup {
   private def log(project: Option[Project], message: String, messageType: MessageType, notification: (String, MessageType) => Notification) = {
     project match {
       case Some(p) if !p.isDisposed && p.isInitialized => notification(message, messageType).notify(p)
-      case None => notification(message, messageType).notify()
+      case None => notification(message, messageType).notify(null)
       case _ => ()
     }
   }
