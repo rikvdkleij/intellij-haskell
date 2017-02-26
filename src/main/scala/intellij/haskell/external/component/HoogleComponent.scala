82063690f6d96b00e0b1fffe0b5f2e273b981b11
zjhmale <zjhmale@gmail.com>
2017-02-26 15:59:58 +0800

    Eliminate hoogle error log

--- src/main/scala/intellij/haskell/external/component/HoogleComponent.scala
+++ src/main/scala/intellij/haskell/external/component/HoogleComponent.scala
@@ -72,7 +72,7 @@ object HoogleComponent {
     ApplicationManager.getApplication.executeOnPooledThread(new Runnable {
       override def run(): Unit = {
         try {
-          StackCommandLine.runCommand(Seq("hoogle", "--rebuild"), project, timeoutInMillis = 10.minutes.toMillis, logErrorAsInfo = true)
+          StackCommandLine.runCommand(Seq("hoogle", "--rebuild"), project, timeoutInMillis = 10.minutes.toMillis, logErrorAsInfo = true, captureOutputToLog = true)
         } finally {
           hoogleAvailable = true
         }
