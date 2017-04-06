87c8de600238ace5ce259d32e00545922302a062
Rik van der Kleij <rikvdkleij@gmail.com>
2017-04-06 15:00:28 +0200

    Check if file is Haskell file and improved. Fix issue #170

--- src/main/scala/intellij/haskell/editor/HaskellCompletionCharFilter.scala
+++ src/main/scala/intellij/haskell/editor/HaskellCompletionCharFilter.scala
@@ -18,17 +18,17 @@ package intellij.haskell.editor
 
 import com.intellij.codeInsight.lookup.CharFilter.Result
 import com.intellij.codeInsight.lookup.{CharFilter, Lookup}
+import intellij.haskell.HaskellFile
 
 class HaskellCompletionCharFilter extends CharFilter {
   def acceptChar(c: Char, prefixLength: Int, lookup: Lookup): CharFilter.Result = {
-    if (!lookup.isCompletion) {
-      null
-    }
-    else {
-      c match {
-        case ',' | ';' | ' ' | '(' | '['  => Result.SELECT_ITEM_AND_FINISH_LOOKUP
-        case _ => Result.ADD_TO_PREFIX
-      }
+    if (lookup == null || lookup.getPsiElement == null) return null
+    val file = lookup.getPsiFile
+    if (!file.isInstanceOf[HaskellFile]) return null
+
+    c match {
+      case ' ' |',' | ';' | ':' | '(' | ')' | '[' | ']' | '{' | '}' => Result.SELECT_ITEM_AND_FINISH_LOOKUP
+      case _ => Result.ADD_TO_PREFIX
     }
   }
 }
