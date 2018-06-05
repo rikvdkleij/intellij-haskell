891e747314e272c738ed176f8cd7ce6b78dd6e1f
Rik van der Kleij <rikvdkleij@gmail.com>
2018-06-05 21:44:24 +0200

    Update Copyright.

--- src/main/scala/intellij/haskell/action/HoogleNavigationAction.scala
+++ src/main/scala/intellij/haskell/action/HoogleNavigationAction.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Rik van der Kleij
+ * Copyright 2014-2018 Rik van der Kleij
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *//*
- * Copyright 2014-2017 Rik van der Kleij
+ * Copyright 2014-2018 Rik van der Kleij
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/scala/intellij/haskell/external/execution/CommandLine.scala
+++ src/main/scala/intellij/haskell/external/execution/CommandLine.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Rik van der Kleij
+ * Copyright 2014-2018 Rik van der Kleij
 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
