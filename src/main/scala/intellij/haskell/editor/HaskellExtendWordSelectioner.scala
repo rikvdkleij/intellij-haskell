178d00c763d08f6def6bf8da2c20f958b702cde8
Rik van der Kleij <rikvdkleij@gmail.com>
2019-03-21 13:35:45 +0100

    Improved extending selection. Fix issue #398.

--- src/main/scala/intellij/haskell/editor/HaskellExtendWordSelectioner.scala
+++ src/main/scala/intellij/haskell/editor/HaskellExtendWordSelectioner.scala
@@ -39,7 +39,7 @@ class HaskellExtendWordSelectioner extends ExtendWordSelectionHandler {
     val startOffset = e.getTextRange.getStartOffset
     val nextEndOffsets = getOffsets(Some(e), ListBuffer.empty[Int], (e: PsiElement) => e.getNextSibling, (e: PsiElement) => e.getTextRange.getEndOffset)
 
-    val prevStartOffsets = getOffsets(HaskellPsiUtil.findQualifiedName(e).flatMap(qe => Option(qe.getPrevSibling)), ListBuffer.empty[Int], (e: PsiElement) => e.getPrevSibling, (e: PsiElement) => e.getTextRange.getStartOffset)
+    val prevStartOffsets = getOffsets(Option(e.getPrevSibling), ListBuffer.empty[Int], (e: PsiElement) => e.getPrevSibling, (e: PsiElement) => e.getTextRange.getStartOffset)
     val lastEndOffset = nextEndOffsets.lastOption.getOrElse(e.getTextRange.getEndOffset)
 
     (nextEndOffsets.map(eo => new TextRange(startOffset, eo)) ++ prevStartOffsets.map(so => new TextRange(so, lastEndOffset))).asJava
