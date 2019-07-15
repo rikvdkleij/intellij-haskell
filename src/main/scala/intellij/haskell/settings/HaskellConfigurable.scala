5f30e4ae36c01630b5ec8ec6c31cf9f3f0ff582d
Rik van der Kleij <rikvdkleij@gmail.com>
2019-07-15 20:15:37 +0200

    Refactor.

--- src/main/scala/intellij/haskell/settings/HaskellConfigurable.scala
+++ src/main/scala/intellij/haskell/settings/HaskellConfigurable.scala
@@ -45,6 +45,7 @@ class HaskellConfigurable extends Configurable {
 
     val settingsPanel = new JPanel(new GridBagLayout())
 
+    settingsPanel.getInsets()
     val listener: DocumentAdapter = (_: DocumentEvent) => {
       isModifiedByUser = true
     }
@@ -53,35 +54,37 @@ class HaskellConfigurable extends Configurable {
     replTimeoutField.getDocument.addDocumentListener(listener)
     extraStackArgumentsField.getDocument.addDocumentListener(listener)
 
-    val base = new GridBagConstraints {
-      insets = new Insets(2, 0, 2, 3)
+    class SettingsGridBagConstraints extends GridBagConstraints {
 
-      def setConstraints(anchor: Int = GridBagConstraints.CENTER, gridx: Int, gridy: Int, weightx: Double = 0, weighty: Double = 0, fill: Int = GridBagConstraints.NONE) = {
+      def setConstraints(anchor: Int = GridBagConstraints.CENTER, gridx: Int, gridy: Int, weightx: Double = 0, weighty: Double = 0, fill: Int = GridBagConstraints.NONE): SettingsGridBagConstraints = {
         this.anchor = anchor
         this.gridx = gridx
         this.gridy = gridy
         this.weightx = weightx
         this.weighty = weighty
         this.fill = fill
+        this.insets = new Insets(2, 0, 2, 3)
         this
       }
     }
 
+    val baseGridBagConstraints = new SettingsGridBagConstraints
+
     def addLabeledControl(row: Int, label: String, component: JComponent): Unit = {
-      settingsPanel.add(new JLabel(label), base.setConstraints(
+      settingsPanel.add(new JLabel(label), baseGridBagConstraints.setConstraints(
         anchor = GridBagConstraints.LINE_START,
         gridx = 0,
         gridy = row
       ))
 
-      settingsPanel.add(component, base.setConstraints(
+      settingsPanel.add(component, baseGridBagConstraints.setConstraints(
         gridx = 1,
         gridy = row,
         fill = GridBagConstraints.HORIZONTAL,
         weightx = 1.0
       ))
 
-      settingsPanel.add(Box.createHorizontalStrut(1), base.setConstraints(
+      settingsPanel.add(Box.createHorizontalStrut(1), baseGridBagConstraints.setConstraints(
         gridx = 2,
         gridy = row,
         weightx = 0.1
@@ -94,7 +97,7 @@ class HaskellConfigurable extends Configurable {
     addLabeledControl(4, ExtraStackArguments, extraStackArgumentsField)
     addLabeledControl(5, "", extraStackArgumentsLabel)
 
-    settingsPanel.add(new JPanel(), base.setConstraints(
+    settingsPanel.add(new JPanel(), baseGridBagConstraints.setConstraints(
       gridx = 0,
       gridy = 7,
       weighty = 10.0
