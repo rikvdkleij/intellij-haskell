d889d05fa1d76b4cbe9417774f265ce16c656ab0
Rik van der Kleij <rikvdkleij@gmail.com>
2020-09-21 19:45:50 +0200

    Import SDK setup validator.

--- src/main/scala/intellij/haskell/notification/HaskellProjectSdkSetupValidator.scala
+++ src/main/scala/intellij/haskell/notification/HaskellProjectSdkSetupValidator.scala
@@ -15,13 +15,13 @@ import intellij.haskell.sdk.HaskellSdkType
 object HaskellProjectSdkSetupValidator {
   final val Instance = new HaskellProjectSdkSetupValidator
 
-  private def preparePopup(project: Project, file: VirtualFile): SdkPopupBuilder = {
-    SdkPopupFactory.newBuilder.withProject(project).withSdkTypeFilter((sdkTypeId: SdkTypeId) => sdkTypeId.isInstanceOf[HaskellSdkType]).updateSdkForFile(file)
+  private def preparePopup(project: Project): SdkPopupBuilder = {
+    SdkPopupFactory.newBuilder.withProject(project).withSdkTypeFilter((sdkTypeId: SdkTypeId) => sdkTypeId.isInstanceOf[HaskellSdkType]).updateProjectSdkFromSelection()
   }
 }
 
 class HaskellProjectSdkSetupValidator extends ProjectSdkSetupValidator {
-  private final val Message = "Haskell SDK is not defined. Please setup Haskell SDK and reopen project."
+  private final val Message = "Haskell SDK is not defined. Please setup Haskell SDK and then reopen project."
 
   override def isApplicableFor(project: Project, file: VirtualFile): Boolean = {
     Option(PsiManager.getInstance(project).findFile(file)) match {
@@ -43,6 +43,6 @@ class HaskellProjectSdkSetupValidator extends ProjectSdkSetupValidator {
   }
 
   override def getFixHandler(project: Project, file: VirtualFile): EditorNotificationPanel.ActionHandler = {
-    HaskellProjectSdkSetupValidator.preparePopup(project, file).buildEditorNotificationPanelHandler
+    HaskellProjectSdkSetupValidator.preparePopup(project).buildEditorNotificationPanelHandler
   }
 }
